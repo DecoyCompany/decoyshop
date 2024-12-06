880473cc16c2e3d565e386931b25b691fdca3473
milksut <altaygokis@gmail.com>
2024-12-06 20:28:19 +0300

    Initial commit

---
+++ src/main/java/com/decoyshop/decoyshop/DecoyshopApplication.java
@@ -0,0 +1,13 @@
+package com.decoyshop.decoyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecoyshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decoyshop/decoyshop/DecoyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decoyshop.decoyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

217cd16bfa5985a44f5563665bd923a0d6f20a2d
Melik Yalcinkaya <melikyalcinkaya55@gmail.com>
2024-12-09 21:49:04 +0300

    urun 'table' created

--- src/main/java/com/decoyshop/entities/urun.java
+++ src/main/java/com/decoyshop/entities/urun.java
@@ -6,12 +6,34 @@ import lombok.Data;
 
 @Entity
 @Data
+
 public class urun
 {
     @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "URUNID")
     private int id;
 
+    @Column(name = "URUN_AD", nullable = false)
+    private String URUN_AD;
 
-    private String isim;
+    @Column(name = "URUN_FIYAT",nullable = false)
+    private double URUN_FIYAT;
+
+    @Column(name = "URUN_PUANI", nullable = false)
+    private float URUN_PUANI;
+
+    @Column(name = "URUN_ETIKETI", nullable = true)
+    private String URUN_ETIKETI;
+
+    @Column(name = "URUN_RESMI" , nullable = true)
+    private String URUN_RESMI;
+
+    @Column(name = "URUN_KATEGORISI", nullable = false)
+    private String URUN_KATEGORISI;
+
+
+
+
 }

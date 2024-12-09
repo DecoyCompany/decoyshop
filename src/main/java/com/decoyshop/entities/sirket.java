1120893c8e091c46aa16c83bf50fc337f579c662
Melik Yalcinkaya <melikyalcinkaya55@gmail.com>
2024-12-10 00:38:06 +0300

    sirket table created

--- src/main/java/com/decoyshop/entities/sirket.java
+++ src/main/java/com/decoyshop/entities/sirket.java
@@ -8,5 +8,27 @@ import lombok.Data;
 @Data
 public class sirket {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SIRKET_ID", nullable = false)
+    private int SIRKET_ID;
+
+    @Column(name = "SIRKET_ISMI", nullable = false)
+    private String SIRKET_ISMI;
+
+
+    @Column(name = "SIRKET_ADRESI", nullable = false)
+    private String SIRKET_ADRESI;
+
+    @Column(name = "ILETISIM_NO", nullable = false)
+    private int ILETISIM_NO;
+
+
+    @Column(name = "ALINAN_SIPARIS_ID", nullable = false)
+    private int ALINAN_SIPARIS_ID;
+
+    @Column(name = "STOCK_ID", nullable = false)
+    private int STOCK_ID;
+
 
 }

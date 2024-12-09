04060ec1005d3f0bf14f4bf91b5ed21c05dee90f
Melik Yalcinkaya <melikyalcinkaya55@gmail.com>
2024-12-09 22:44:00 +0300

    Kategori table created

--- src/main/java/com/decoyshop/entities/kategorı.java
+++ src/main/java/com/decoyshop/entities/kategorı.java
@@ -1,4 +1,23 @@
 package com.decoyshop.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class kategorı {
+
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "KATEGORI_ID",nullable = false)
+    private int id;
+
+    @Column(name = "KATEGORI_ISMI", nullable = false)
+    private String KATEGORI_ISMI;
+
+    @Column(name = "KATEGORI_STOK",nullable = false)
+    private int KATEGORI_STOK;
+
+
 }
